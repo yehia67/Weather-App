5af24466fe1fd665afbadbda38d2c7fe2dbe2bcf
yehia67 <yehiatarek67@gmail.com>
2019-01-24 19:05:52 +0200

    yarab

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Weather App</title>
+</head>
+
+<body>
+
+	<center><br><br>
+		<form method="GET" action="Result.php">
+		<h1>Type City and Country</h1>
+			<input type="text" name="q" placeholder="Cairo,Egypt" required>
+			<input type="submit" name="submit">
+		</form>
+	</center>
+</body>
+</html>
